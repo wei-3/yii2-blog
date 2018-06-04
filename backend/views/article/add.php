587d09e47002968925704b72631ccab2d23ffe80
wjl <867160866@qq.com>
2018-06-04 15:51:58 +0800

    百度编辑器

--- backend/views/article/add.php
+++ backend/views/article/add.php
@@ -9,8 +9,15 @@ echo $form->field($model_article,'title')->textInput();
 echo $form->field($model_article,'intro')->textarea(['rows'=>3]);
 echo $form->field($model_article,'article_category_id')->dropDownList(\yii\helpers\ArrayHelper::map($model_categorys,'id','name'));
 echo $form->field($model_article,'sort')->textInput(['type'=>'number']);
-echo $form->field($model_detail,'content')->textarea(['rows'=>5]);
-//echo $form->field($model_detail,'content')->widget('')
+// echo $form->field($model_detail,'content')->textarea(['rows'=>5]);
+echo $form->field($model_detail,'content')->widget('kucha\ueditor\UEditor',[
+    'clientOptions'=>[
+        //编辑区域大小
+        'initialFrameHeight' => '200',
+        //设置语言
+        'lang' =>'en', //中文为 zh-cn
+    ]
+]);
 echo $form->field($model_article,'status',['inline'=>true])->radioList(['隐藏','显示']);
 echo \yii\bootstrap\Html::submitButton('提交',['class'=>'btn btn-primary']);
 \yii\bootstrap\ActiveForm::end();
