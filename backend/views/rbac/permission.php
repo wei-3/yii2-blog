86533a0064a1d9614e711fb655f54b368198ec0f
wjl <867160866@qq.com>
2018-07-12 14:57:50 +0800

    添加返回按钮

--- backend/views/rbac/permission.php
+++ backend/views/rbac/permission.php
@@ -1,4 +1,11 @@
 <?php
+?>
+    <nav aria-label="...">
+        <ul class="pager">
+            <li class="previous"><a href="<?=\yii\helpers\Url::to(['rbac/index-permission'])?>"><span aria-hidden="true">&larr;</span>&nbsp;返回权限列表</a></li>
+        </ul>
+    </nav>
+<?php
 $form=\yii\bootstrap\ActiveForm::begin();
 echo $form->field($model,'name')->textInput();
 echo $form->field($model,'description')->textInput();
