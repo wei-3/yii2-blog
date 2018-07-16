bd537139243b54bf0839f94617334bc2030aa784
wjl <867160866@qq.com>
2018-07-16 15:10:22 +0800

    删除菜单

--- backend/controllers/MenuController.php
+++ backend/controllers/MenuController.php
@@ -20,6 +20,7 @@ class MenuController extends \yii\web\Controller
         return $this->render('index',['models'=>$models,'pager'=>$pager]);
     }
 
+    //添加菜单
     public function actionAdd(){
         $model=new Menu();
         $request=\Yii::$app->request;
@@ -35,4 +36,34 @@ class MenuController extends \yii\web\Controller
         return $this->render('add',['model'=>$model]);
     }
 
+    //修改菜单
+    public function actionEdit($id){
+        $model=Menu::findOne(['id'=>$id]);
+        $request=\Yii::$app->request;
+        if($request->isPost){
+            $model->load($request->post());
+            if($model->validate()){
+                $model->save();
+                \Yii::$app->session->setFlash('success','修改成功!');
+                return $this->redirect(['menu/index']);
+            }
+        }
+        return $this->render('add',['model'=>$model]);
+    }
+
+    //删除菜单
+    public function actionDel(){
+        $id=\Yii::$app->request->post('id');
+        $model=Menu::findOne(['id'=>$id]);
+        $person=Menu::find()->where(['parent_id'=>$id])->all();
+//        var_dump($person);exit();
+        if ($model){
+            if ($person){
+               return '该菜单下有子菜单';
+            }else{
+                $model->delete();
+                return 'success';
+            }
+        }
+    }
 }
--- backend/views/menu/index.php
+++ backend/views/menu/index.php
@@ -5,13 +5,18 @@
     <tr>
         <th>名称</th>
         <th>路由</th>
-        <th>排序</th>
+        <th style="width: 200px">排序</th>
+        <th>操作</th>
     </tr>
     <?php foreach ($models as $model):?>
         <tr data-id="<?=$model->id?>">
             <td><?=$model->name?></td>
             <td><?=$model->url?></td>
             <td><?=$model->sort?></td>
+            <td>
+                <a href="<?=\yii\helpers\Url::to(['menu/edit','id'=>$model->id])?>" class="btn btn-default"><span class="glyphicon glyphicon-pencil"></span></a>
+                <a href="javascript:;" class="btn btn-default del_btn"><span class="glyphicon glyphicon-trash"></span></a>
+            </td>
         </tr>
     <?php endforeach;?>
 </table>
@@ -19,3 +24,26 @@
 echo \yii\widgets\LinkPager::widget([
     'pagination'=>$pager,
 ]);
+//注册js代码
+$del_url=\yii\helpers\Url::to(['menu/del']);
+$this->registerJs(new \yii\web\JsExpression(
+    <<<JS
+        $('.del_btn').click(function() {
+          if(confirm('确定要删除吗?')){
+               var tr=$(this).closest('tr');
+               var id=tr.attr('data-id');
+               $.post("{$del_url}",{id:id},function(data) {
+                 if(data=='success'){
+                       tr.fadeToggle();
+                       alert('删除成功');
+                       
+                 }else{
+                       alert(data);
+                 }
+               })
+          }
+        })
+JS
+
+
+));
