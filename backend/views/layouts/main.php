4cf105dce3cf4f767a7ffa483f6953becde56677
wjl <867160866@qq.com>
2018-07-16 16:22:14 +0800

    导航显示

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -36,20 +36,46 @@ AppAsset::register($this);
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
+//        ['label' => '首页', 'url' => ['/site/index']],
+        ['label' => '用户管理','items'=>[
+            ['label' => '用户列表', 'url' => ['/user/index']],
+            ['label' => '添加用户', 'url' => ['/user/add']],
+        ]],
+        ['label' => '文章管理','items'=>[
+            ['label' => '文章列表', 'url' => ['/article/index']],
+            ['label' => '添加文章', 'url' => ['/article/add']],
+        ]],
+        ['label' => '评论管理','items'=>[
+            ['label' => '评论列表', 'url' => ['/comment/index']],
+            ['label' => '添加评论', 'url' => ['/comment/add']],
+        ]],
+        ['label' => '文章分类管理','items'=>[
+            ['label' => '文章分类列表', 'url' => ['/article-category/index']],
+            ['label' => '添加文章分类', 'url' => ['/article-category/add']],
+        ]],
+        ['label' => 'RBAC管理','items'=>[
+            ['label' => '权限列表', 'url' => ['/rbac/index-permission']],
+            ['label' => '添加权限', 'url' => ['/rbac/add-permission']],
+            ['label' => '角色列表', 'url' => ['/rbac/index-role']],
+            ['label' => '添加角色', 'url' => ['/rbac/add-role']],
+        ]],
+        ['label' => '菜单管理','items'=>[
+            ['label' => '菜单列表', 'url' => ['/menu/index']],
+            ['label' => '添加菜单', 'url' => ['/menu/add']],
+        ]],
     ];
-    if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-    } else {
-        $menuItems[] = '<li>'
-            . Html::beginForm(['/site/logout'], 'post')
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout']
-            )
-            . Html::endForm()
-            . '</li>';
-    }
+//    if (Yii::$app->user->isGuest) {
+//        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+//    } else {
+//        $menuItems[] = '<li>'
+//            . Html::beginForm(['/site/logout'], 'post')
+//            . Html::submitButton(
+//                'Logout (' . Yii::$app->user->identity->username . ')',
+//                ['class' => 'btn btn-link logout']
+//            )
+//            . Html::endForm()
+//            . '</li>';
+//    }
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => $menuItems,
--- backend/views/menu/add.php
+++ backend/views/menu/add.php
@@ -1,4 +1,11 @@
 <?php
+?>
+    <nav aria-label="...">
+        <ul class="pager">
+            <li class="previous"><a href="<?=\yii\helpers\Url::to(['menu/index'])?>"><span aria-hidden="true">&larr;</span>&nbsp;返回评论首页</a></li>
+        </ul>
+    </nav>
+<?php
 $form=\yii\bootstrap\ActiveForm::begin();
 echo $form->field($model,'name')->textInput();
 echo $form->field($model,'parent_id')->dropDownList(\backend\models\Menu::getParents(),['prompt'=>'==请选择上级菜单==']);
